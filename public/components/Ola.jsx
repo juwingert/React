47ee1b6161ab863b393bacf76692fad42520576c
Juliana Wingert <87996722+juwingert@users.noreply.github.com>
2023-01-17 21:44:24 -0300

    Initial commit

---
+++ public/components/Ola.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Ola.css";
+
+import Cards from "./card/Cards";
+
+export default function Ola() {
+  return (
+    <>
+      <Cards />
+    </>
+  );
+}
---
+++ public/components/card/Cards.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./Cards.css";
+
+export default function Cards() {
+  return (
+    <div className="cartao">
+      <h1 className="titulo">Today is my birthday!</h1>
+      <h3 className="subtitulo">Come to celebrate with me</h3>
+      <p className="texto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione animi
+        debitis temporibus tempore consequuntur unde reiciendis odit
+        consectetur? Tempora provident quas nulla. Illo dolorem culpa
+        consectetur ullam, nostrum assumenda explicabo!
+      </p>
+    </div>
+  );
+}
